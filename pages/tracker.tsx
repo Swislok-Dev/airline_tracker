181191373892a4adcc61cea623bc0b957e34db3d
Swislok-Dev <cmarker4@gmail.com>
2022-11-26 20:56:05 -0600

    remove Layout from page

--- pages/tracker.tsx
+++ pages/tracker.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Layout from "../components/Layout";
 import axios, { AxiosResponse } from "axios";
 import * as functions from "../components/flights/functions.js";
 import FlightItem from "../components/flights/FlightItem";
@@ -14,7 +13,6 @@ function Tracker() {
     setFlights(flightsArray);
     const nextFlight = functions.findNextFlight(flightsArray);
     setNextFlight(nextFlight);
-    // console.log("Data from getFlightInfo", nextFlight);
     return nextFlight;
   };
 
@@ -22,49 +20,41 @@ function Tracker() {
     setIdent(e.target.value);
   };
 
-  // const listFlights = (ident: string) => {
-  //   const nextFlight = getFlightInfo(ident);
-  //   console.log(nextFlight);
-  //   return nextFlight;
-  // };
-
   const handleOnSubmit = (e: any) => {
     e.preventDefault();
     return getFlightInfo(ident);
   };
 
   return (
-    <Layout title="Find A Flight">
-      <section>
-        <form
-          onSubmit={(e) => handleOnSubmit(e)}
-          className=" m-auto mb-4 grid max-w-sm grid-cols-1 gap-2 px-2 sm:max-w-sm md:max-w-md"
+    <section>
+      <form
+        onSubmit={(e) => handleOnSubmit(e)}
+        className=" m-auto mb-4 grid max-w-sm grid-cols-1 gap-2 px-2 sm:max-w-sm md:max-w-md"
+      >
+        <label htmlFor="ident">Flight Number</label>
+        <input
+          type="text"
+          name="ident"
+          autoFocus
+          className="w-auto rounded border-2 border-red-500 text-center uppercase"
+          required
+          onChange={(e) => onChange(e)}
+          value={ident}
+          placeholder="UA4"
+        />
+        <button
+          type="submit"
+          value="Find Flight"
+          className="rounded bg-red-500 text-white active:bg-red-400 "
         >
-          <label htmlFor="ident">Flight Number</label>
-          <input
-            type="text"
-            name="ident"
-            autoFocus
-            className="w-auto rounded border-2 border-red-500 text-center uppercase"
-            required
-            onChange={(e) => onChange(e)}
-            value={ident}
-            placeholder="UA4"
-          />
-          <button
-            type="submit"
-            value="Find Flight"
-            className="rounded bg-red-500 text-white active:bg-red-400 "
-          >
-            Search
-          </button>
-        </form>
-        <div>{flights == null ? <h2>Find a Flight</h2> : null}</div>
-        {flights == null ? null : (
-          <FlightItem nextFlight={nextFlight} flightNumber={ident} />
-        )}
-      </section>
-    </Layout>
+          Search
+        </button>
+      </form>
+      <div>{flights == null ? <h2>Find a Flight</h2> : null}</div>
+      {flights == null ? null : (
+        <FlightItem nextFlight={nextFlight} flightNumber={ident} />
+      )}
+    </section>
   );
 }
 
