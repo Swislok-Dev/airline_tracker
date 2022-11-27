60cfc971333f7364798c0a67af416316a92a0ad7
Swislok-Dev <cmarker4@gmail.com>
2022-11-26 20:55:45 -0600

    remove title Props && center navbar element/header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,29 +3,23 @@ import Head from "next/head";
 import Link from "next/link";
 
 type Props = {
-  title: string;
   children: any;
 };
 
-const Layout: React.FC<Props> = ({ title, children }) => {
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <Head>
-        <title>{title ? title + " - Flight Tracker" : "Flight Tracker"}</title>
+        <title>Flight Tracker</title>
         <meta name="description" content="Flight tracking application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex-min-h-screen flex-col justify-between">
         <header className="sticky mb-4 w-full bg-white shadow">
-          <nav className="mx-4 flex h-12 items-center justify-between ">
+          <nav className="mx-4 flex h-12 items-center justify-center ">
             <div>
               <Link href="/" className=" text-lg font-bold">
                 Flight Tracker
-              </Link>
-            </div>
-            <div>
-              <Link href="/tracker" className="">
-                Tracker
               </Link>
             </div>
           </nav>
