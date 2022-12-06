ee76e34219d9c39844e859b19e9d8f893e90899b
Swislok-Dev <cmarker4@gmail.com>
2022-12-06 11:59:07 -0600

    simplify search function to last arrived flight

--- components/flights/functions.js
+++ components/flights/functions.js
@@ -1,25 +1,18 @@
-const now = new Date();
-
 export const convertISOTimeToDateTime = (time) => {
   return new Date(time);
 };
 
 export const findNextFlight = (flightsArray) => {
-  let currentFlight;
   const { data } = flightsArray;
 
   for (let i = 0; i < data.length; i++) {
-    let flightTime = convertISOTimeToDateTime(data[i].scheduled_off);
     let nextFlight = data[i + 1];
-    if (flightTime > now && nextFlight.status !== "Scheduled") {
-      if (nextFlight.status.includes("En Route")) {
-        currentFlight = nextFlight;
-        return currentFlight;
-      }
-      currentFlight = data[i];
+    let currentFlight = data[i];
+
+    if (nextFlight.status.includes("Arrived")) {
+      return currentFlight;
     }
   }
-  return currentFlight;
 };
 
 export const convertDate = (date) => {
