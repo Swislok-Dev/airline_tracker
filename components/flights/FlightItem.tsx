484464dc18b9c18867bd9f03c7389dcd39fd9a85
Swislok-Dev <cmarker4@gmail.com>
2022-12-08 10:32:46 -0600

    new show page layout

--- components/flights/FlightItem.tsx
+++ components/flights/FlightItem.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { convertDate } from "../../components/flights/functions.js";
+import { getDate, getTime } from "../../components/flights/functions.js";
 
 function FlightItem({ nextFlight, flightNumber }: any) {
+  console.log(nextFlight);
   const getOriginAirportName = () => {
     return nextFlight.origin.name;
   };
@@ -17,21 +18,20 @@ function FlightItem({ nextFlight, flightNumber }: any) {
           <p>Please check your flight number</p>
         </div>
       ) : (
-        <div>
-          <div>
-            <h2
-              className="m-4 text-2xl font-bold"
-              title="this is an internal reference number and may differ from your input"
-            >
-              Flight
-              <br />
-              {flightNumber === nextFlight.ident
-                ? nextFlight.ident.toUpperCase()
-                : nextFlight.ident_iata.toUpperCase()}
-            </h2>
-            <span className="mt-2 text-xl font-semibold">Flight Status</span>
-            <h3 className="mb-2 text-xl font-semibold">{nextFlight.status}</h3>
-
+        <div className="border-2 border-red-300">
+          <div className="flex flex-col text-left shadow">
+            <div className=" bg-lime-300 p-4">
+              <h3>{nextFlight.status}</h3>
+            </div>
+            <div className="bg-blue-400 p-4">
+              <h2 title="this is an internal reference number and may differ from your input">
+                {flightNumber === nextFlight.ident
+                  ? nextFlight.ident.toUpperCase()
+                  : nextFlight.ident_iata.toUpperCase()}
+              </h2>
+            </div>
+          </div>
+          <div className="flex">
             <span className="text-xl font-semibold">
               {nextFlight.status === "Scheduled"
                 ? null
@@ -39,100 +39,89 @@ function FlightItem({ nextFlight, flightNumber }: any) {
             </span>
           </div>
 
-          <div className="items-auto m-auto mt-4 grid max-w-2xl grid-rows-2 justify-center gap-2 px-2  sm:grid-cols-2   ">
-            <div className="m-2 border p-4">
-              <h3 className="mb-4 text-xl font-bold">Departure</h3>
-              <table className="border-separate border-spacing-1">
-                <tbody>
-                  <tr>
-                    <td>Scheduled Takeoff</td>
-                    <td>{convertDate(nextFlight.scheduled_off)}</td>
-                  </tr>
-                  <tr>
-                    <td>Actual Takeoff</td>
-                    <td>
-                      {nextFlight.actual_off == null
-                        ? "TBD"
-                        : convertDate(nextFlight.actual_off)}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Airport</td>
-                    <td>{getOriginAirportName()}</td>
-                  </tr>
-                  <tr>
-                    <td>City</td>
-                    <td>{nextFlight.origin.city}</td>
-                  </tr>
-                  <tr>
-                    <td>Terminal</td>
-                    <td>
-                      {nextFlight.terminal_origin == null
-                        ? "TBD"
-                        : nextFlight.terminal_origin}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Gate</td>
-                    <td>
-                      {nextFlight.gate_origin == null
-                        ? "TBD"
-                        : nextFlight.gate_origin}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          <div>
+            <div className="card">
+              <h2 className="iata-code">{nextFlight.origin.code_iata}</h2>
+              <span className="airport-name">{getOriginAirportName()}</span>
+              <h3>Departure Times</h3>
+              <span>{getDate(nextFlight.scheduled_off)}</span>
+              <div className="flight-times">
+                <div>
+                  <h3>Scheduled</h3>
+                  <span>{getTime(nextFlight.scheduled_off)}</span>
+                </div>
+                <div>
+                  {nextFlight.status.includes("En Route") ? (
+                    <div>
+                      <h3>Actual</h3>
+
+                      <span>{getTime(nextFlight.actual_off)}</span>
+                    </div>
+                  ) : (
+                    <div>
+                      <h3>Estimated</h3>
+                      <span>{getTime(nextFlight.estimated_off)}</span>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className="terminal-info">
+                <div className="terminal">
+                  <h3>Terminal</h3>
+                  <span>{nextFlight.terminal_origin}</span>
+                </div>
+                <div className="gate">
+                  <h3>Gate</h3>
+                  <span>{nextFlight.gate_origin}</span>
+                </div>
+              </div>
             </div>
 
-            <div className="m-2  border p-4">
-              <h3 className="mb-4 text-xl font-bold">Arrival</h3>
-              <table className="border-separate border-spacing-1">
-                <tbody>
-                  {nextFlight.scheduled_on === nextFlight.estimated_on ? (
-                    <tr>
-                      <td>Scheduled Arrival</td>
-                      <td>{convertDate(nextFlight.scheduled_on)}</td>
-                    </tr>
+            <div className="card">
+              <h2 className="iata-code">{nextFlight.destination.code_iata}</h2>
+              <span className="airport-name">
+                {getDestinationAirportName()}
+              </span>
+              <h3>Arrival Times</h3>
+              <span>{getDate(nextFlight.scheduled_on)}</span>
+              <div className="flight-times">
+                <div>
+                  <h3>Scheduled</h3>
+                  <span>{getTime(nextFlight.scheduled_on)}</span>
+                </div>
+                <div>
+                  {nextFlight.status.includes("Arrived") ? (
+                    <div>
+                      <h3>Actual</h3>
+
+                      <span>{getTime(nextFlight.actual_on)}</span>
+                    </div>
                   ) : (
-                    <tr>
-                      <td>Estimated Arrival</td>
-                      <td>{convertDate(nextFlight.estimated_on)}</td>
-                    </tr>
+                    <div>
+                      <h3>Estimated</h3>
+                      <span>{getTime(nextFlight.estimated_on)}</span>
+                    </div>
                   )}
-                  <tr>
-                    <td>Actual Arrival</td>
-                    <td>
-                      {nextFlight.actual_on == null
-                        ? "TBD"
-                        : convertDate(nextFlight.actual_on)}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Airport</td>
-                    <td>{getDestinationAirportName()}</td>
-                  </tr>
-                  <tr>
-                    <td>City</td>
-                    <td>{nextFlight.destination.city}</td>
-                  </tr>
-                  <tr>
-                    <td>Terminal</td>
-                    <td>
-                      {nextFlight.terminal_destination == null
-                        ? "TBD"
-                        : nextFlight.terminal_destination}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Gate</td>
-                    <td>
-                      {nextFlight.gate_destination == null
-                        ? "TBD"
-                        : nextFlight.gate_destination}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                </div>
+              </div>
+
+              <div className="terminal-info">
+                <div className="terminal">
+                  <h3>Terminal</h3>
+                  <span>{nextFlight.terminal_destination}</span>
+                </div>
+                <div className="gate">
+                  <h3>Gate</h3>
+                  <span>{nextFlight.gate_destination}</span>
+                </div>
+                {nextFlight.baggage_claim !== null ? (
+                  <div className="baggage-claim">
+                    <h3>Baggage</h3>
+                    <span>{nextFlight.baggage_claim}</span>
+                  </div>
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
