64b296e39834a4ce4943758187fdb5644cf49070
cm@MBA <cmarker4@gmail.com>
2022-12-21 13:08:23 -0600

    create select flight selector

--- components/flights/FlightItem.tsx
+++ components/flights/FlightItem.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { getDate, getTime } from "../../components/flights/functions.js";
 import * as functions from "../flights/functions";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
-function FlightItem({ nextFlight, flightNumber }: any) {
+function FlightItem({ currentFlight, flightNumber, flights }: any) {
+  const [displayCurrentFlight, setDisplayCurrentFlight] =
+    useState(currentFlight);
+
+  const getFlightIndex = (flight: any) => {
+    return flights.data.indexOf(flight);
+  };
+
+
+  let index = getFlightIndex(displayCurrentFlight);
+
+  const getPreviousFlight = () => {
+    if (index == 0) {
+      null;
+    } else {
+      setDisplayCurrentFlight(flights.data[index - 1]);
+    }
+  };
+  const getNextFlight = () => {
+    if (index !== flights.data.length - 1) {
+      setDisplayCurrentFlight(flights.data[index + 1]);
+    }
+  };
+
   const getOriginAirportName = () => {
-    return nextFlight.origin.name;
+    return displayCurrentFlight.origin.name;
   };
   const getDestinationAirportName = () => {
-    return nextFlight.destination.name;
+    return displayCurrentFlight.destination.name;
   };
 
   const checkNullValue = (flight: any) => {
@@ -20,7 +44,7 @@ function FlightItem({ nextFlight, flightNumber }: any) {
 
   return (
     <>
-      {nextFlight === undefined ? (
+      {currentFlight === undefined ? (
         <div className="text-white">
           <p>Flight not found</p>
           <p>Please check your flight number</p>
@@ -31,57 +55,71 @@ function FlightItem({ nextFlight, flightNumber }: any) {
             <div
               className={
                 "flight-status " +
-                functions.checkFlightStatus(nextFlight.status)
+                functions.checkFlightStatus(displayCurrentFlight.status)
               }
             >
-              <h3>{`${nextFlight.status}`}</h3>
+              <h3>{`${displayCurrentFlight.status}`}</h3>
               <span></span>
             </div>
-            <div className="bg-blue-400 p-4">
+            <div className="flex justify-between bg-blue-400 p-4">
               <h2 title="this is an internal reference number and may differ from your input">
-                {flightNumber === nextFlight.ident
-                  ? nextFlight.ident.toUpperCase()
-                  : nextFlight.ident_iata.toUpperCase()}
+                {flightNumber === currentFlight.ident
+                  ? currentFlight.ident.toUpperCase()
+                  : currentFlight.ident_iata.toUpperCase()}
               </h2>
             </div>
           </div>
           <div id="progress" className="flex text-white">
-            <h2>{nextFlight.origin.code_iata}</h2>
+            <h2>{displayCurrentFlight.origin.code_iata}</h2>
             <div id="progress-bar">
               <span
                 style={{
                   visibility: "visible",
-                  width: `${Number(nextFlight.progress_percent)}%`,
+                  width: `${Number(displayCurrentFlight.progress_percent)}%`,
                 }}
               >
                 progress
               </span>
             </div>
-            <h2>{nextFlight.destination.code_iata}</h2>
+            <h2>{displayCurrentFlight.destination.code_iata}</h2>
+          </div>
+
+          <div className="m-3 flex justify-between px-4 text-2xl text-white">
+            <button onClick={getNextFlight}>{<FaArrowCircleLeft />}</button>
+            {displayCurrentFlight == currentFlight ? (
+              <p>Current Flight</p>
+            ) : (
+              <p>{displayCurrentFlight._id}</p>
+            )}
+            <button onClick={getPreviousFlight}>
+              {<FaArrowCircleRight />}
+            </button>
           </div>
 
           <div>
             <div className="card">
-              <h2 className="iata-code">{nextFlight.origin.code_iata}</h2>
+              <h2 className="iata-code">
+                {displayCurrentFlight.origin.code_iata}
+              </h2>
               <span className="airport-name">{getOriginAirportName()}</span>
               <h3>Departure Times</h3>
-              <span>{getDate(nextFlight.scheduled_off)}</span>
+              <span>{getDate(displayCurrentFlight.scheduled_off)}</span>
               <div className="flight-times">
                 <div>
                   <h3>Scheduled</h3>
-                  <span>{getTime(nextFlight.scheduled_off)}</span>
+                  <span>{getTime(displayCurrentFlight.scheduled_off)}</span>
                 </div>
                 <div>
-                  {nextFlight.status.includes("En Route") ? (
+                  {displayCurrentFlight.status.includes("En Route") ? (
                     <div>
                       <h3>Actual</h3>
 
-                      <span>{getTime(nextFlight.actual_off)}</span>
+                      <span>{getTime(displayCurrentFlight.actual_off)}</span>
                     </div>
                   ) : (
                     <div>
                       <h3>Estimated</h3>
-                      <span>{getTime(nextFlight.estimated_off)}</span>
+                      <span>{getTime(displayCurrentFlight.estimated_off)}</span>
                     </div>
                   )}
                 </div>
@@ -90,38 +128,44 @@ function FlightItem({ nextFlight, flightNumber }: any) {
               <div className="terminal-info">
                 <div className="terminal">
                   <h3>Terminal</h3>
-                  <span>{checkNullValue(nextFlight.terminal_origin)}</span>
+                  <span>
+                    {checkNullValue(displayCurrentFlight.terminal_origin)}
+                  </span>
                 </div>
                 <div className="gate">
                   <h3>Gate</h3>
-                  <span>{checkNullValue(nextFlight.gate_origin)}</span>
+                  <span>
+                    {checkNullValue(displayCurrentFlight.gate_origin)}
+                  </span>
                 </div>
               </div>
             </div>
 
             <div className="card">
-              <h2 className="iata-code">{nextFlight.destination.code_iata}</h2>
+              <h2 className="iata-code">
+                {displayCurrentFlight.destination.code_iata}
+              </h2>
               <span className="airport-name">
                 {getDestinationAirportName()}
               </span>
               <h3>Arrival Times</h3>
-              <span>{getDate(nextFlight.scheduled_on)}</span>
+              <span>{getDate(displayCurrentFlight.scheduled_on)}</span>
               <div className="flight-times">
                 <div>
                   <h3>Scheduled</h3>
-                  <span>{getTime(nextFlight.scheduled_on)}</span>
+                  <span>{getTime(displayCurrentFlight.scheduled_on)}</span>
                 </div>
                 <div>
-                  {nextFlight.status.includes("Arrived") ? (
+                  {displayCurrentFlight.status.includes("Arrived") ? (
                     <div>
                       <h3>Actual</h3>
 
-                      <span>{getTime(nextFlight.actual_on)}</span>
+                      <span>{getTime(displayCurrentFlight.actual_on)}</span>
                     </div>
                   ) : (
                     <div>
                       <h3>Estimated</h3>
-                      <span>{getTime(nextFlight.estimated_on)}</span>
+                      <span>{getTime(displayCurrentFlight.estimated_on)}</span>
                     </div>
                   )}
                 </div>
@@ -130,16 +174,20 @@ function FlightItem({ nextFlight, flightNumber }: any) {
               <div className="terminal-info">
                 <div className="terminal">
                   <h3>Terminal</h3>
-                  <span>{checkNullValue(nextFlight.terminal_destination)}</span>
+                  <span>
+                    {checkNullValue(displayCurrentFlight.terminal_destination)}
+                  </span>
                 </div>
                 <div className="gate">
                   <h3>Gate</h3>
-                  <span>{checkNullValue(nextFlight.gate_destination)}</span>
+                  <span>
+                    {checkNullValue(displayCurrentFlight.gate_destination)}
+                  </span>
                 </div>
-                {nextFlight.baggage_claim !== null ? (
+                {displayCurrentFlight.baggage_claim !== null ? (
                   <div className="baggage-claim">
                     <h3>Baggage</h3>
-                    <span>{nextFlight.baggage_claim}</span>
+                    <span>{displayCurrentFlight.baggage_claim}</span>
                   </div>
                 ) : null}
               </div>
