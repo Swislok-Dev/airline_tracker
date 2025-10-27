4b244df783e4a25ae2613961abe52a50e5fbb6ac
Swislok-Dev <cmarker4@gmail.com>
2025-10-27 11:08:36 -0500

    setup search form and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="search-form">
+	<form class="form-control">
+		<label for="ident">Please enter a flight number</label>
+		<div id="search-input">
+			<input
+				type="text"
+				name="ident"
+				required
+				placeholder="ua4"
+			/>
+			<button
+				id="search-button"
+				type="submit"
+				value="Find Flight"
+			>
+				Search
+			</button>
+		</div>
+	</form>
+</section>
+
+<script>
+
+</script>
+
+<style>
+	form {
+		margin: 2rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 20px;
+	}
+
+	#search-input {
+		display: flex;
+		gap: 5px;
+		max-width: 200vw;
+	}
+
+	#search-input input {
+		max-width: 100%;
+		margin: auto;
+		padding: 5px;
+		text-align: center;
+		text-transform: capitalize;
+	}
+
+	label {
+		color: white;
+	}
+</style>
