178d53550175adc93b24427bd898c811325e421c
Swislok-Dev <cmarker4@gmail.com>
2025-10-29 16:19:19 -0500

    formatting

--- src/components/searchForm.svelte
+++ src/components/searchForm.svelte
@@ -1,38 +1,46 @@
-<section id="search-form">
-	<form class="form-control">
-		<label for="ident">Please enter a flight number</label>
-		<div id="search-input">
-			<input
-				type="text"
-				name="ident"
-				required
-				placeholder="ua4"
-			/>
-			<button
-				id="search-button"
-				type="submit"
-				value="Find Flight"
-			>
-				Search
-			</button>
-		</div>
-	</form>
-</section>
+<!-- <section id="search-form"> -->
+<form class="form-control">
+	<label for="ident">Please enter a flight number</label>
+	<div id="search-input">
+		<input
+			type="text"
+			name="ident"
+			required
+			placeholder="ua4"
+		/>
+		<button
+			id="search-button"
+			type="submit"
+			value="Find Flight"
+		>
+			Search
+		</button>
+	</div>
+</form>
+
+<!-- </section> -->
 
 <style>
-	form {
-		margin: 2rem;
+	.form-control {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex-direction: column;
-		gap: 20px;
+		margin: auto;
+		width: 100%;
+		gap: 2rem;
+	}
+	form {
+		padding: 0 0.5rem;
+    width: 75%;
+		max-width: 28rem;
 	}
 
 	#search-input {
 		display: flex;
+		flex-direction: row;
+		flex-grow: 1;
 		gap: 5px;
-		max-width: 200vw;
 	}
 
 	#search-input input {
