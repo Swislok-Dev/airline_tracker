41fa4b2fd91b94827bc32ca70cbdbf33b74b5dab
Swislok-Dev <cmarker4@gmail.com>
2025-11-04 08:24:04 -0600

    simplify landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,45 @@
 <script>
+	import { resolve } from '$app/paths';
 	import FlightTicket from '../components/flights/flightTicket.svelte';
 	import SearchForm from '../components/searchForm.svelte';
 </script>
 
-<SearchForm />
-<FlightTicket />
+<header>
+	<nav>
+		<h1>
+			<a href={resolve('/')}>Flight Tracker</a>
+		</h1>
+	</nav>
+</header>
+
+<main>
+	<div>
+		<SearchForm />
+		<FlightTicket />
+	</div>
+</main>
+
+<style>
+	header {
+		padding: 1rem 0;
+		text-align: center;
+		margin-bottom: 1rem;
+		color: white;
+	}
+
+	main {
+		margin: auto;
+		width: 100%;
+		max-width: 768px;
+	}
+
+	h1 {
+		font-size: 1.875rem;
+		line-height: 2.25rem;
+		font-weight: 700;
+	}
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+</style>
