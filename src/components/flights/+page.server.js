eb1092959a0bd2f1636930f52bda0c0bf973bbbd
Swislok-Dev <cmarker4@gmail.com>
2025-11-11 08:24:33 -0600

    add getFlightStatus function

---
+++ src/components/flights/+page.server.js
@@ -0,0 +1,18 @@
+const flightScheduled = {
+	Scheduled: 'scheduled',
+	'Scheduled / Delayed': 'scheduled delayed',
+	Delayed: 'delayed',
+	'En Route / Delayed': 'enroute delayed',
+	'En Route / On Time': 'enroute on-time',
+	Taxiing: 'taxiing',
+	'Taxiing / Delayed': 'taxiingin delayed',
+	'En Route': 'enroute'
+};
+
+export function getFlightStatus(status) {
+  for (const key in flightScheduled) {
+    if status.contains(key) {
+      console.log(key)
+    }
+  }
+}
