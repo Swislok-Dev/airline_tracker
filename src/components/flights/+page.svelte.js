e4196b62f4ea0dc4f4199d249d2a9d7f67413497
Swislok-Dev <cmarker4@gmail.com>
2025-11-26 00:30:02 -0600

    create functions to find current flight

--- src/components/flights/+page.svelte.js
+++ src/components/flights/+page.svelte.js
@@ -1,6 +1,6 @@
 const flightScheduled = {
-	Scheduled: 'scheduled',
-	'Scheduled / Delayed': 'scheduled delayed',
+	Scheduled: 'Scheduled',
+	'Scheduled / Delayed': 'Scheduled delayed',
 	Delayed: 'delayed',
 	'En Route / Delayed': 'enroute delayed',
 	'En Route / On Time': 'enroute on-time',
@@ -13,6 +13,37 @@ export function getFlightStatus(status) {
 	for (const key in flightScheduled) {
 		if (status.contains(key)) {
 			console.log(key);
+		}
+	}
+}
+
+const nextScheduledFlight = (currentFlight, nextFlight) => {
+	console.log('nextScheduledFlight()');
+	if (
+		nextFlight.actuall_off === null &&
+		!nextFlight.status.includes('Cancelled')
+	) {
+		return false;
+	}
+	if (
+		nextFlight.actual_off !== null &&
+		flightScheduled.includes(nextFlight.status)
+	) {
+		return false;
+	}
+	return true;
+};
+
+export function findCurrentFlight(flightsArray) {
+	console.log('findCurrentFlight()');
+	const flights = flightsArray['flights'];
+
+	for (let i = 0; i < flights.length; i++) {
+		let nextFlight = flights[i + 1];
+		let currentFlight = flights[i];
+
+		if (nextScheduledFlight(currentFlight, nextFlight)) {
+			return currentFlight;
 		}
 	}
 }
