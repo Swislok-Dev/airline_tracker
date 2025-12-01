310a2d0a6dae2dec0e6ddcbe2ac8f1234f43a433
Swislok-Dev <cmarker4@gmail.com>
2025-12-01 14:13:45 -0600

    clean up

--- src/routes/api/flight/[ident]/+server.js
+++ src/routes/api/flight/[ident]/+server.js
@@ -4,8 +4,6 @@ export async function GET({ params }) {
 	console.log('calling api');
 	const aeroapiURI = `https://aeroapi.flightaware.com/aeroapi/flights/${params.ident}`;
 
-  // return new Response(JSON.stringify({message: "You got it"}))
-	console.log('api called');
 
 	const res = await fetch(aeroapiURI, {
 		// mode: 'no-cors',
@@ -15,16 +13,16 @@ export async function GET({ params }) {
 	});
 
 	if (!res.ok) {
-    console.log("GET method failed", res)
+		console.log('GET method failed', res);
 		return new Response(
 			JSON.stringify({ message: 'Not OK', status: 400 })
 		);
 	}
 
 	const data = await res.json();
-  if (res.ok) {
-    console.log("Data has been secured!")
-  }
+	if (res.ok) {
+		console.log('Data has been secured!');
+	}
 
 	return new Response(JSON.stringify(data), {
 		status: 200
