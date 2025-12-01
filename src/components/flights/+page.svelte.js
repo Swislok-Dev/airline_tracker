37472c718203a9a00a26f79f9616352a21a64609
Swislok-Dev <cmarker4@gmail.com>
2025-12-01 13:52:08 -0600

    get the current flight to show page

--- src/components/flights/+page.svelte.js
+++ src/components/flights/+page.svelte.js
@@ -1,13 +1,13 @@
-const flightScheduled = {
-	Scheduled: 'Scheduled',
-	'Scheduled / Delayed': 'Scheduled delayed',
-	Delayed: 'delayed',
-	'En Route / Delayed': 'enroute delayed',
-	'En Route / On Time': 'enroute on-time',
-	Taxiing: 'taxiing',
-	'Taxiing / Delayed': 'taxiingin delayed',
-	'En Route': 'enroute'
-};
+const flightScheduled = [
+	'Scheduled',
+	'Scheduled / Delayed',
+	'Delayed',
+	'En Route / Delayed',
+	'En Route / On Time',
+	'Taxiing',
+	'Taxiing / Delayed',
+	'En Route'
+];
 
 export function getFlightStatus(status) {
 	for (const key in flightScheduled) {
@@ -17,50 +17,57 @@ export function getFlightStatus(status) {
 	}
 }
 
-const nextScheduledFlight = (currentFlight, nextFlight) => {
-	console.log('nextScheduledFlight()');
-	console.log({ currentFlight, nextFlight });
+const nextScheduledFlight = (currentFlight, prevFlight) => {
+	console.log('currentFlight.status', currentFlight.status);
+	console.log('prevFlight.status', prevFlight.status);
+	console.log(new Date().toISOString());
+
+	console.log({ currentFlight, prevFlight });
+
 	if (
-		nextFlight.actuall_off === null &&
-		!nextFlight.status.includes('Cancelled')
+		prevFlight.actual_off == null &&
+		!prevFlight.status.includes('Cancelled')
 	) {
 		return false;
 	}
 	if (
-		nextFlight.actual_off !== null &&
-		flightScheduled.includes(nextFlight.status)
+		prevFlight.actual_off != null &&
+		flightScheduled.includes(prevFlight.status)
 	) {
 		return false;
 	}
+	console.log('Previous flight status:', prevFlight.status);
+	console.log('Current flight status:', currentFlight.status);
+
+
+
 	return true;
 };
 
-export function findCurrentFlight(flightsArray) {
-	console.log('findCurrentFlight()', flightsArray);
+export const findCurrentFlight = (flightsArray) => {
 	const flights = flightsArray['flights'];
 
 	for (let i = 0; i < flights.length; i++) {
-		let nextFlight = flights[i + 1];
+		let prevFlight = flights[i + 1];
 		let currentFlight = flights[i];
 
-		if (nextScheduledFlight(currentFlight, nextFlight)) {
+		if (nextScheduledFlight(currentFlight, prevFlight)) {
 			return currentFlight;
 		}
 	}
-}
-
+};
 
 const dateString = (date) => {
-  return new Date(date);
+	return new Date(date);
 };
 
 export const getTime = (date) => {
-  const time = dateString(date).toLocaleTimeString();
-  return time;
+	const time = dateString(date).toLocaleTimeString();
+	return time;
 };
 
 export const getDate = (date) => {
-  if (date) {
-    return dateString(date).toDateString();
-  }
+	if (date) {
+		return dateString(date).toDateString();
+	}
 };
